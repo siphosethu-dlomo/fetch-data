bcd25b54509d86a998c20545be115824a2b09a9e
Siphosethu Dlomo <siphosethugdlomo@gmail.com>
2024-05-14 01:24:04 +0200

    fetch data with custom useFetch hook

--- fetch/src/components/TripList.jsx
+++ fetch/src/components/TripList.jsx
@@ -1,47 +1,47 @@
-import { useState, useEffect, useCallback } from 'react'
-import { Button } from "flowbite-react";
+import { useState } from 'react'
+import { Button, Spinner, Alert } from "flowbite-react";
+import { useFetch } from '../hooks/useFetch';
 
 export default function TripList() {
-  const [trips, setTrips] = useState([])
-
   const [url, setUrl] = useState('http://localhost:3000/trips')
 
-  const fetchTrips = useCallback(async () => {
-    const response = await fetch(url)
-    const json = await response.json()
-    setTrips(json)
-  }, [url])
-  
-  useEffect(() => {
-    fetchTrips()
-  }, [url], fetchTrips)
+  const { data: trips, isPending, error } = useFetch(url)
 
   return (
     <div>
       <>
         <h2 className='text-center mt-4 font-bold text-3xl'>Trip List</h2>
         <div className='grid place-content-center text-center mt-10'>
-        <div className='flex  justify-center mb-10'>
+          <div className='flex  justify-center mb-10'>
             <Button
-             className='bg-blue-500 mr-5'
-             onClick={() => setUrl('http://localhost:3000/trips?loc=europe')}
+              className='bg-blue-500 mr-5'
+              onClick={() => setUrl('http://localhost:3000/trips?loc=europe')}
             >
               European Trips
             </Button>
             <Button
-             className='bg-purple-500'
-             onClick={() => setUrl('http://localhost:3000/trips')}
+              className='bg-purple-500'
+              onClick={() => setUrl('http://localhost:3000/trips')}
             >
               All Trips
             </Button>
           </div>
-          {trips.map(trip => (
+          {/* loading state */}
+          {isPending && <div className="text-center mb-4">
+            <Spinner color="info" className='h-20 w-20' />
+          </div>}
+          {/* loading state */}
+          {error &&
+            <Alert color="failure">
+              <span className="font-medium">Error: </span>Could not fetch the data
+          </Alert>}
+
+          {trips && trips.map(trip => (
             <div key={trip.id} className='shadow-lg hover:shadow-blue-500 text-white rounded w-96 mb-10 p-5 bg-gradient-to-r from-cyan-500 to-blue-500'>
               <h1 className='font-semibold text-2xl mb-5'>{trip.title}</h1>
               <p className=''>{trip.price}</p>
             </div>
           ))}
-
         </div>
       </>
     </div>
--- fetch/src/hooks/useFetch.jsx
+++ fetch/src/hooks/useFetch.jsx
@@ -1,19 +1,44 @@
 import { useState, useEffect} from "react"
 
-const useFetch = (url) => {
+export const useFetch = (url) => {
   const [data, setData] = useState(null)
+  // loading state
+  const [isPending, setIsPending] = useState(false)
+  const [error, setError] = useState(null)
 
   useEffect(() => {
+    const controller = new AbortController()
+
     const fetchData = async () => {
-      const res = await fetch(url)
-      const json = await res.json()
-      setData(json)
+      try {
+        setIsPending(true)
+
+        const res = await fetch(url, { signal: controller.signal })
+        if(!res.ok) {
+          throw new Error(res.statusText)
+        }
+        const json = await res.json()
+
+        setIsPending(false)
+        setData(json)
+        setError(null)
+      } catch (error) {
+        if(error.message === "AbortError") {
+          console.log("the fetch was aborted");
+        } else {
+          setIsPending(false)
+          setError('Could not fetch the data')
+        }
+       
+      }
     }
 
     fetchData()
+    return () => {
+      controller.abort()
+    }
+
   }, [url])
 
-  return { data }
+  return { data, isPending, error }
 }
-
-export default useFetch
